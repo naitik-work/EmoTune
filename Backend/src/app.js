35294ff111e7f79c8517557e8b0a9e76544a992a
naitik-cmd <naitikchs16@gmail.com>
2026-03-02 20:19:51 +0530

    Google mediapipeline setup done

---
+++ Backend/src/app.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express= require("express");
+const cookieParser= require("cookie-parser");
+const authRouter= require("./routes/auth.routes");
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api/auth", authRouter);
+
+
+
+module.exports= app;
+
